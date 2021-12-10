dc6e8a496d750f6993f9478434d4b2272262e840
AhmedA <alkhatib_ahmed1995@hotmail.com>
2021-12-10 17:33:09 -0500

    generate markdown table of contents added

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,8 +12,19 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-  
+  return `
+  # ${data.title}
+
+  ## Description
+  ${description}
+
+  ## Table of contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributions](#contributions)
+  * [Tests](#tests)
+  * [Questions](#questions)
 `;
 }
 
